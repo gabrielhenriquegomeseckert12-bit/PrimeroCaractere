f0875be687c0f74078f1e1a885a01c46d56f8b99
gabrielhenriquegomeseckert12-bit <gabrielhenriquegomeseckert12@gmail.com>
2026-09-18 21:58:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿﻿Console.WriteLine("--- Primero Caractere ---\n");
+
+Console.Write("Digite algo...: ");
+string textoDigitado = Console.ReadLine()!;
+
+Console.WriteLine(textoDigitado.Trim().Substring(0, 1));
